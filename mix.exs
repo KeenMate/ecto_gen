773e1f22c74ea6dd0ee32d0cec1709cee663dca1
Filip Jakab <jakabfilip.fj@gmail.com>
2022-11-30 02:41:44 +0100

    v0.9.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoGen.MixProject do
   def project do
     [
       app: :ecto_gen,
-      version: "0.9.1",
+      version: "0.9.2",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
